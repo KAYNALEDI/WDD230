aea07413ddd3fc1244241583b430058b43628d53
KAYNALEDI <72366409+KAYNALEDI@users.noreply.github.com>
2023-04-26 21:30:31 -0400

    Create dates.js

---
+++ dates.js
@@ -0,0 +1,3 @@
+document.querySelector("#timestamp").innerHTML = String("Last updated: ") + String(document.lastModified);
+document.querySelector("#copyright").innerHTML = String("&copy ") + new Date().getFullYear() + String(" Nokwazi Kunene    Florida");
+© 2023 GitHub, Inc.

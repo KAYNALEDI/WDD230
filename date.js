900d6b420aab2d52c9d4fcca35be667f0b5f2764
KAYNALEDI <72366409+KAYNALEDI@users.noreply.github.com>
2023-05-08 14:24:19 -0400

    Update date.js

--- date.js
+++ date.js
@@ -1,2 +1,9 @@
-let nLastModif = Date.parse(document.lastModified);
+document.getElementById("year").innerHTML = new Date().getFullYear();
 
+var x = document.lastModified;
+document.getElementById("date").innerHTML = x;
+
+function toggleMenu() {
+   
+    document.getElementById("primaryNav").classList.toggle("hide");
+}

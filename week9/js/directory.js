3e6858218895b51f2af4cd83f579e01b140db274
KAYNALEDI <72366409+KAYNALEDI@users.noreply.github.com>
2023-07-10 19:58:40 -0400

    Update directory.js

--- week9/js/directory.js
+++ week9/js/directory.js
@@ -171,3 +171,18 @@ function switchView(view) {
 //  populate the company list and grid
 populateCompanyList();
 populateCompanyGrid();
+
+
+// Get the current year
+const currentYear = new Date().getFullYear();
+
+ document.addEventListener("DOMContentLoaded", function () {
+            // Set current year
+            var currentYear = new Date().getFullYear();
+            document.getElementById("currentyear").textContent = currentYear;
+
+            // Set last updated date
+            var lastUpdatedDate = new Date(document.lastModified);
+            var options = { year: 'numeric', month: 'long', day: 'numeric' };
+            document.getElementById("last-updated").textContent = lastUpdatedDate.toLocaleDateString(undefined, options);
+        });
